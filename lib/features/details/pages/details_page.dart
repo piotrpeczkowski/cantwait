ab6c05c161af011a25f8d0a360347a442ae344e7
Piotr Pęczkowski <peczkowski.piotrek@gmail.com>
2023-01-06 20:31:05 +0100

    adjust item view at details screen

--- lib/features/details/pages/details_page.dart
+++ lib/features/details/pages/details_page.dart
@@ -28,100 +28,85 @@ class DetailsPage extends StatelessWidget {
             }
             return ListView(
               padding: const EdgeInsets.symmetric(
-                vertical: 20,
+                vertical: 10,
               ),
               children: [
-                _ListViewItem(
-                  itemModel: itemModel,
-                ),
-              ],
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class _ListViewItem extends StatelessWidget {
-  const _ListViewItem({
-    Key? key,
-    required this.itemModel,
-  }) : super(key: key);
-
-  final ItemModel itemModel;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 10,
-        horizontal: 30,
-      ),
-      child: Container(
-        decoration: const BoxDecoration(
-          color: Colors.black12,
-        ),
-        child: Column(
-          children: [
-            Container(
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                image: DecorationImage(
-                  image: NetworkImage(
-                    itemModel.imageURL,
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
                   ),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Row(
-              children: [
-                Expanded(
                   child: Container(
-                    margin: const EdgeInsets.all(10),
-                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      color: Colors.black12,
+                    ),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        Text(
-                          itemModel.title,
-                          style: const TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
+                        Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            color: Colors.black12,
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                itemModel.imageURL,
+                              ),
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                        const SizedBox(height: 10),
-                        Text(
-                          itemModel.releaseDateFormatted(),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                margin: const EdgeInsets.all(10),
+                                padding: const EdgeInsets.all(10),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Text(
+                                      itemModel.title,
+                                      style: const TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      itemModel.releaseDateFormatted(),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.white70,
+                              ),
+                              margin: const EdgeInsets.all(10),
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    itemModel.daysLeft(),
+                                    style: const TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const Text('days left'),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
                   ),
                 ),
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white70,
-                  ),
-                  margin: const EdgeInsets.all(10),
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      Text(
-                        itemModel.daysLeft(),
-                        style: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text('days left'),
-                    ],
-                  ),
-                ),
               ],
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
